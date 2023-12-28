6181773db5c99f5461ca563dc786e9dce405ec8f
trhotd <92685009+Dissident11@users.noreply.github.com>
2023-12-28 16:31:27 +0100

    Update main.lua
    Added offset, more zoom options and made the code clearer

--- main.lua
+++ main.lua
@@ -38,45 +38,51 @@ draw_lines = false
 draw_points = true
 even_function_mode = false
 odd_function_mode = false
-show_axis = false
+show_axis = true
 
 --variables for the slider
 a = 0
 b = 1
 
 --zoom
-zooms = {1, 2, 3, 4, 5}
-zoom_index = 1
+zooms = {0.1, 0.25, 0.5, 0.75, 0.8, 1, 2, 3, 4, 5, 8, 10}
+zoom_index = 6
 zoom = zooms[zoom_index]
-dx = 1 / zoom --the width of a pixel based on the zoom
-rounding = dx / 2
+d = 1 / zoom --the width of a pixel based on the zoom
+rounding = d / 2
 w_pressed = false
 s_pressed = false
+
+--offsetting the graph
+dx = 0
+dy = 0
+x_increment = 5
+y_increment = 5
 
 function pesto.update(dt)
 
     --zoom in
-    if pesto.keyboard.isDown("w") and w_pressed == false and zoom_index < #zooms then
+    if pesto.keyboard.isDown("up") and up_pressed == false and zoom_index < #zooms then
         zoom_index = zoom_index + 1
-        w_pressed = true
+        up_pressed = true
     end
-    if not pesto.keyboard.isDown("w") then
-        w_pressed = false
+    if not pesto.keyboard.isDown("up") then
+        up_pressed = false
     end
 
     --zoom out
-    if pesto.keyboard.isDown("s") and s_pressed == false and zoom_index > 1 then
+    if pesto.keyboard.isDown("down") and down_pressed == false and zoom_index > 1 then
         zoom_index = zoom_index - 1
-        s_pressed = true
+        down_pressed = true
     end
-    if not pesto.keyboard.isDown("s") then
-        s_pressed = false
+    if not pesto.keyboard.isDown("down") then
+        down_pressed = false
     end
 
     --zoom
     zoom = zooms[zoom_index]
-    dx = 1 / zoom
-    rounding = dx / 2
+    d = 1 / zoom
+    rounding = d / 2
 
     --slider, can be done in lots of ways
     if auto_slider then
@@ -89,10 +95,61 @@ function pesto.update(dt)
         a = a + b*0.01
     end
 
+    --offset
+    dx = 0
+    dy = 0
+
+    --offset up
+    if pesto.keyboard.isDown("w") and w_pressed == false then
+        dy = dy - y_increment
+        w_pressed = true
+    end
+    if not pesto.keyboard.isDown("w") then
+        w_pressed = false
+    end
+
+    --offset down
+    if pesto.keyboard.isDown("s") and s_pressed == false then
+        dy = dy + y_increment 
+        s_pressed = true
+    end
+    if not pesto.keyboard.isDown("s") then
+        s_pressed = false
+    end
+
+    --offset left
+    if pesto.keyboard.isDown("a") and a_pressed == false then
+        dx = dx - x_increment
+        a_pressed = true
+    end
+    if not pesto.keyboard.isDown("a") then
+        a_pressed = false
+    end
+
+    --offset right
+    if pesto.keyboard.isDown("d") and d_pressed == false then
+        dx = dx + x_increment
+        d_pressed = true
+    end
+    if not pesto.keyboard.isDown("d") then
+        d_pressed = false
+    end
+
+    --reset offset
+    if pesto.mouse.isPressed(2) then
+        center_x = screen_width / 2
+        center_y = screen_height / 2
+    end
+
+
+    --offsetting the axes
+    center_x = center_x + dx
+    center_y = center_y + dy
+
     --graph parameters
     points = {}
     graph_points = {}
-    range = {-3, 3}
+    range = {-1000, 1000}
     step = 1
 
     --create all graph points
@@ -101,9 +158,10 @@ function pesto.update(dt)
         if zoom == 1 then
             y = round(y) --rounding the y to the closest integer, by default it's made with floor
         else   
-            adding = round(previous_integer_d(y) / dx) --rounding y to the closest point
-            y = floor(y) + adding * dx
-        end    
+            adding = round(previous_integer_d(y) / d) --rounding y to the closest point
+            y = floor(y) + adding * d
+        end  
+
         --distance with respect to a fixed point
         distance = math.sqrt((x - origin[1])^2 + (y - origin[2])^2)
 
@@ -138,18 +196,10 @@ end
 
 function pesto.draw()
 
-    pesto.graphics.text(#points, 1, 1)
-    pesto.graphics.text(zoom, 1, 15)
+    --show zoom
+    pesto.graphics.text("ZOOM: ".. (zoom * 100).. "%", 1, 1)
 
-    for v=1, #points, 1 do
-        pesto.graphics.text((graph_points[v][1].. " ".. graph_points[v][2]), 1, 20 + (v)*15)
-        pesto.graphics.text((points[v][1].. " ".. points[v][2]), 700, 20 + (v)*15)
-    end
-
-    pesto.graphics.text( -0.3 + 0.3, 10, 20)
-
-
-    --x and y axis
+    --x and y axes
     if show_axis then
         pesto.graphics.rectangle(center_x, 0, 1, screen_height)
         pesto.graphics.rectangle(0, center_y, screen_width, 1)
@@ -167,6 +217,7 @@ function pesto.draw()
     if draw_lines then
         
         --use with even function, of course
+        --lines between points are drown from and to top-left corner before origin, from and to top-right corner after it
         if even_function_mode then
             for i = 1, #points - 1, 1 do
                 if points[i][1] < center_x and points[i+1][1] <= center_x then
@@ -177,6 +228,7 @@ function pesto.draw()
             end
         
         --use with odd function, but not with all as for some it may just worsen the graph
+        --lines between points are drown from and to top-left corner before origin, from and to bottom-right corner after it
         elseif odd_function_mode then
             for i = 1, #points - 1, 1 do
                 if points[i][1] < center_x and points[i+1][1] <= center_x then
@@ -185,7 +237,8 @@ function pesto.draw()
                     pesto.graphics.line(points[i][1] + 1, points[i][2] + 1, points[i + 1][1] + 1, points[i + 1][2] + 1)
                 end
             end
-
+        
+        --normal way with which lines are drown, from and to every top-left corner of points
         else
             for i = 1, #points - 1, 1 do
                 pesto.graphics.line(points[i][1], points[i][2], points[i + 1][1], points[i + 1][2])
